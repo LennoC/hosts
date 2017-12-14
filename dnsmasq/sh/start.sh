0e2df27aa49035c5d71178b204ea659a27d6a23d
LennoC <30518666+LennoC@users.noreply.github.com>
2017-12-14 18:07:44 +0800

    不再下载AD hosts文件
    有需要者自行去掉注释，建议先去了解vokins/yhosts项目

--- dnsmasq/sh/start.sh
+++ dnsmasq/sh/start.sh
@@ -2,10 +2,10 @@
 
 #创建文件夹
 mkdir -p /etc/storage/dnsmasq/dns/conf
-##下载hosts规则
-cd /etc/storage/dnsmasq/dns
-wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts
-sed -i -e '/gay\|uvwxyz\|XiaoQiang\|alfredapp\|dxomark/d' -i -e "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" hosts
+##下载hosts规则 /20171214 去掉hosts文件，有需要者自行去掉注释，建议先去了解vokins/yhosts项目后再决定
+#cd /etc/storage/dnsmasq/dns
+#wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts
+#sed -i -e '/gay\|uvwxyz\|XiaoQiang\|alfredapp\|dxomark/d' -i -e "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" hosts
 
 #下载dnsmasq规则
 cd /etc/storage/dnsmasq/dns/conf
